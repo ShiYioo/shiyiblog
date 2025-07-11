cb16f6387c63fe2c1034649541b2b2791b3b29b1
shiyio <3401187804@qq.com>
2025-07-11 12:17:22 +0800

    fix

--- .vuepress/client.ts
+++ .vuepress/client.ts
@@ -2,11 +2,8 @@
 
 import { h ,createApp , onUnmounted} from 'vue'
 import { useRouter } from 'vue-router'
-import Live2DModel from './components/Live2DModel.vue'
 import { defineClientConfig } from '@vuepress/client'
 import ResponsiveSidebar from './components/ResponsiveSidebar.vue'
-import  MusicPlayer  from './components/music-player.vue'
-import APlayer from 'vue3-aplayer'
 import ReadTime from './components/ReadTime.vue'
 import {
   useReadingTimeData,
@@ -15,28 +12,23 @@ import {
 } from '@vuepress/plugin-reading-time/client'
 export default defineClientConfig({
   enhance({ app }) {
-    app.component('MusicPlayer', MusicPlayer)
-    app.component('Live2DModel', Live2DModel)
-    app.component('APlayer', APlayer)
   },
   setup() {
     const router = useRouter()
-    // console.log('当前路由:', router.currentRoute.value.path)
-
-    // const isMobile = /mobile/i.test(window.navigator.userAgent)
-    // if (!isMobile) {
-    //   const container = document.createElement('div')
-    //   document.body.appendChild(container)
-    //   createApp(h(Live2DModel)).mount(container)
-    // }
     
   const sidebarContainer = document.createElement('div')
     document.body.appendChild(sidebarContainer)
     createApp(h(ResponsiveSidebar)).mount(sidebarContainer)
 
-    const musiccontainer = document.createElement('div')
-  document.body.appendChild(musiccontainer)
-  createApp(h(MusicPlayer)).mount(musiccontainer)
+   if (typeof window !== 'undefined') {
+      import('./components/music-player.vue').then(({ default: MusicPlayer }) => {
+        const musiccontainer = document.createElement('div')
+        document.body.appendChild(musiccontainer)
+        createApp(h(MusicPlayer)).mount(musiccontainer)
+      }).catch(err => {
+        console.error('Failed to load MusicPlayer:', err)
+      })
+    }
 
   let readTimeApp: any = null
 let readTimeContainer: HTMLElement | null = null
